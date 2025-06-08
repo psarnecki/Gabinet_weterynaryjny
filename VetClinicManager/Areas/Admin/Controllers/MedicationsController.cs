ca39f9dad56f939a28e6d238860233a3d02e0901
igororlinski <igor.orlinski04@gmail.com>
2025-06-08 23:22:19 +0200

    Fixed view errors in more views

--- VetClinicManager/Areas/Admin/Controllers/MedicationsController.cs
+++ VetClinicManager/Areas/Admin/Controllers/MedicationsController.cs
@@ -8,7 +8,6 @@ using VetClinicManager.Services;
 namespace VetClinicManager.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [Authorize(Roles = "Admin")]
     public class MedicationsController : Controller
     {
         private readonly IMedicationService _medicationService;
@@ -19,6 +18,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         }
 
         // GET: Admin/Medications
+        [Authorize(Roles = "Admin, Receptionist, Vet")]
         public async Task<IActionResult> Index()
         {
             var medicationListDtos = await _medicationService.GetAllMedicationsAsync();
@@ -26,6 +26,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         }
 
         // GET: Admin/Medications/Details/5
+        [Authorize(Roles = "Admin, Receptionist, Vet")]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -44,6 +45,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         }
 
         // GET: Admin/Medications/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             return View(new MedicationCreateDto());
@@ -52,6 +54,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         // POST: Admin/Medications/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Create(MedicationCreateDto createDto)
         {
             if (!ModelState.IsValid)
@@ -65,6 +68,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         }
 
         // GET: Admin/Medications/Edit/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -85,6 +89,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         // POST: Admin/Medications/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Edit(int id, MedicationEditDto editDto)
         {
             if (id != editDto.Id)
@@ -121,6 +126,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         }
 
         // GET: Admin/Medications/Delete/5
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -141,6 +147,7 @@ namespace VetClinicManager.Areas.Admin.Controllers
         // POST: Admin/Medications/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             await _medicationService.DeleteMedicationAsync(id);
